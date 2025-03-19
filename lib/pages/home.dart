a32a122d4e2990b657520ffd84ada4aee137376b
sommovir <luca.coraci@gmail.com>
2025-03-19 17:26:03 +0100

    spacer and space between

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,12 +15,62 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.amber,
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Text(
-          title ?? 'Titolo al centro',
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        color: Colors.grey.shade300,
+        child: Center(
+          child: SizedBox(
+            height: 550,
+            width: 400,
+            child: Card(
+              color: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          color: Colors.red,
+                          child: Text(
+                            title ?? 'Titolo al centro',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          color: Colors.blue,
+                          child: Text(
+                            title ?? 'Titolo al centro',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          title ?? 'Titolo al centro',
+                          style: TextStyle(fontSize: 24),
+                        ),
+
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        bottom: 20,
+                      ),
+                      child: Text(
+                        title ?? 'Logout',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
