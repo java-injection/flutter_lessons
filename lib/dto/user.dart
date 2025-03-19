d0d23c10ec1ebdd0a7f5f5216be3222a2eea2ca3
sommovir <luca.coraci@gmail.com>
2025-03-19 16:51:32 +0100

    constructors demo

--- lib/dto/user.dart
+++ lib/dto/user.dart
@@ -1,5 +1,35 @@
+class User {
+  final int id;
+  final String name;
+  final int age;
+  final String? soprannome;
 
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    this.soprannome,
+  });
 
-class User{
+  void greet() {
+    print("Hello $name");
+  }
 
+  @override
+  String toString() => '''
+    id: $id,
+    name: $name,
+    age: $age,
+    soprannome: ${soprannome ?? ""},
+    ''';
+}
+
+void main() {
+  User user = User(
+    id: 9,
+    age: 30,
+    name: "Luca",
+  );
+
+   print("provo a stampare tutto l'oggetto user2:\n$user");
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'dto/user.dart';
 
 void main() {
   runApp(const App());
